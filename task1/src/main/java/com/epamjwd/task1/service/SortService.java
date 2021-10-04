6849ab110439d633a1ace295973fd30fbf8f2897
Us10n <68775862+Us10n@users.noreply.github.com>
2021-10-04 19:14:38 +0300

    Update SortService.java

--- task1/src/main/java/com/epamjwd/task1/service/SortService.java
+++ task1/src/main/java/com/epamjwd/task1/service/SortService.java
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
 package com.epamjwd.task1.service;
 
 import com.epamjwd.task1.entity.DoubleArray;
-import com.epamjwd.task1.exception.DoubleArrayException;
 
 public interface SortService {
     public DoubleArray bubbleSort(DoubleArray customArray, SortDirection direction);
     public DoubleArray selectSort(DoubleArray customArray, SortDirection direction);
     public DoubleArray shellSort(DoubleArray customArray, SortDirection direction);
 }
-=======
-package com.epamjwd.task1.service;
-
-import com.epamjwd.task1.entity.CustomArray;
-import com.epamjwd.task1.entity.DoubleArray;
-
-public interface SortService {
-    public CustomArray bubbleSort(DoubleArray customArray, SortDirection direction);
-    public CustomArray selectSort(DoubleArray customArray, SortDirection direction);
-    public CustomArray inserSort(DoubleArray customArray, SortDirection direction);
-}
->>>>>>> 33dda93079ed769fe037e304cdc43bd0d99737dc
